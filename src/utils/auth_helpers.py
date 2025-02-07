54b99408a1b2ba792b7e536a652384dd182f993f
Jenil Ghevariya <jenilghevariya000@gmail.com>
2025-02-07 17:45:57 +0530

    `Added logic to handle expiration time for JWT tokens when current time is after 2 AM.`

--- src/utils/auth_helpers.py
+++ src/utils/auth_helpers.py
@@ -13,6 +13,10 @@ def decode_auth_token(auth_token):
 def create_jwt_token(identity):
     now = datetime.datetime.now()
     tomorrow_2am = now.replace(hour=2, minute=0, second=0, microsecond=0)
+    # If the current time is after 2 AM, set the expiration to 2 AM the next day
+    if now > tomorrow_2am:
+        tomorrow_2am += datetime.timedelta(days=1)
+    
     expires_delta = tomorrow_2am - now
     access_token = create_access_token(identity=identity, expires_delta=expires_delta)
     return access_token
